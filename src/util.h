5185f63f5385fc1bfa62b9225f134e0b68530e45
shenglin-alex <a182820998@gmail.com>
2019-01-03 14:38:18 +0800

    change progress bar style

--- src/util.h
+++ src/util.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+#define PBSTR "***************************************************************"
 #define PBWIDTH 60
 
 void print_progress(double ratio) {
